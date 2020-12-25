02feefeb36e0c738ed684d1824438a1de628d580
c-brugo <carlobrugo97@gmail.com>
2020-12-25 13:00:21 +0100

    pacs dataset object added

---
+++ Homework3-PACS-master/pacs_dataset.py
@@ -0,0 +1,168 @@
+from torchvision.datasets import VisionDataset
+
+from PIL import Image
+
+import os
+import os.path
+import sys
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+
+def has_file_allowed_extension(filename: str, extensions: Tuple[str, ...]) -> bool:
+    """Checks if a file is an allowed extension.
+
+    Args:
+        filename (string): path to a file
+        extensions (tuple of strings): extensions to consider (lowercase)
+
+    Returns:
+        bool: True if the filename ends with one of given extensions
+    """
+    return filename.lower().endswith(extensions)
+
+
+def is_image_file(filename: str) -> bool:
+    """Checks if a file is an allowed image extension.
+
+    Args:
+        filename (string): path to a file
+
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    return has_file_allowed_extension(filename, IMG_EXTENSIONS)
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+def make_dataset_image_folder(
+    directory: str,
+    class_to_idx: Dict[str, int],
+) -> List[Tuple[str, int]]:
+    instances = []
+    directory = os.path.expanduser(directory)
+    
+    for target_class in sorted(class_to_idx.keys()):
+        class_index = class_to_idx[target_class]
+        target_dir = os.path.join(directory, target_class)
+        if not os.path.isdir(target_dir):
+            continue
+        for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+            for fname in sorted(fnames):
+                path = os.path.join(root, fname)
+                if is_image_file(path):
+                    item = path, class_index
+                    instances.append(item)
+    return instances
+
+def make_dataset_pacs(
+    directory: str,
+    class_to_idx: Dict[str, int],
+) -> List[Tuple[str, int]]:
+    instances = []
+
+    
+    root_directory = directory.split("/")[0]
+    #print(root_directory)
+    root_directory = os.path.expanduser(root_directory)
+    #print(root_directory)
+
+    #print(directory)
+    directory = os.path.expanduser(directory)
+    #print(directory)
+
+    for target_class in sorted(class_to_idx.keys()):
+        class_index = class_to_idx[target_class]
+        target_dir = os.path.join(directory, target_class)
+        if not os.path.isdir(target_dir):
+            continue
+        for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+            for fname in sorted(fnames):
+                path = os.path.join(root, fname)
+                if is_image_file(path):
+                    item = path, class_index
+                    instances.append(item)
+    return instances
+
+
+
+class PACS(VisionDataset):
+    def __init__(self, root, transform=None, target_transform=None):
+        super(PACS, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        classes, class_to_idx = self._find_classes(self.root)
+        samples = make_dataset_pacs(self.root, class_to_idx)
+        if len(samples) == 0:
+            msg = "Found 0 files in subfolders of: {}\n".format(self.root)
+            raise RuntimeError(msg)
+
+        #self.loader = loader
+        #self.extensions = extensions
+
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+
+        '''
+        - Here you should implement the logic for reading the splits files and accessing elements
+        - If the RAM size allows it, it is faster to store all data in memory
+        - PyTorch Dataset classes use indexes to read elements
+        - You should provide a way for the __getitem__ method to access the image-label pair
+          through the index
+        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
+        '''
+
+    def _find_classes(self, dir: str) -> Tuple[List[str], Dict[str, int]]:
+        """
+        Finds the class folders in a dataset.
+
+        Args:
+            dir (string): Root directory path.
+
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+
+        Ensures:
+            No class is a subdirectory of another.
+        """
+        #print(dir)
+        classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
+    def __getitem__(self, index):
+        '''
+        __getitem__ should access an element through its index
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        '''
+
+        path, label = self.samples[index] # Provide a way to access image and label via index
+                           # Image should be a PIL Image
+                           # label can be int
+        image = pil_loader(path)
+        # Applies preprocessing when accessing the image
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+
+        return image, label
+
+    def __len__(self):
+        '''
+        The __len__ method returns the length of the dataset
+        It is mandatory, as this is used by several other components
+        '''
+        length = len(self.samples) # Provide a way to get the length (number of elements) of the dataset
+        return length
